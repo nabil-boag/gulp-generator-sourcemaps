1f091f0cd9f1f993579a80d76aafa9d805dae1dc
Nabil Boag <nabilboag@gmail.com>
2016-04-07 22:59:48 +0100

    First

---
+++ src/app/index.module.ts
@@ -0,0 +1,27 @@
+/// <reference path="../../typings/main.d.ts" />
+
+import { config } from './index.config';
+import { runBlock } from './index.run';
+import { MainController } from './main/main.controller';
+import { GithubContributor } from '../app/components/githubContributor/githubContributor.service';
+import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
+import { acmeNavbar } from '../app/components/navbar/navbar.directive';
+import { acmeMalarkey } from '../app/components/malarkey/malarkey.directive';
+
+declare var malarkey: any;
+declare var moment: moment.MomentStatic;
+
+module tempoCardPicker {
+  'use strict';
+
+  angular.module('tempoCardPicker', ['toastr'])
+    .constant('malarkey', malarkey)
+    .constant('moment', moment)
+    .config(config)
+    .run(runBlock)
+    .service('githubContributor', GithubContributor)
+    .service('webDevTec', WebDevTecService)
+    .controller('MainController', MainController)
+    .directive('acmeNavbar', acmeNavbar)
+    .directive('acmeMalarkey', acmeMalarkey);
+}
